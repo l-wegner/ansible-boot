1ffa45dbde27724a781a6f8deca130d4e62ab66a
Lars Wegner <lars.wegner.devs@gmail.com>
2024-02-23 16:33:43 +0100

    Rework tasks for localhost to run on ubuntu

--- create-user.sh
+++ create-user.sh
@@ -2,6 +2,7 @@
 echo "Enter user name: " && read USER_NM && \
 echo "Enter password for $USER_NM user:" && read -s USER_PW && \
 echo "Enter password for ansible user:" && read -s TOOR_PW && \
+echo "Enter UID and GID for ansible user (e.g. 1001): " && read TOOR_UID
 
 export USER_NM
 if [ ! -z "$USER_PW" ]; then
@@ -9,6 +10,10 @@ if [ ! -z "$USER_PW" ]; then
 fi
 if [ ! -z "$TOOR_PW" ]; then
   export TOOR_PW_HASH=$(echo $TOOR_PW | mkpasswd -m sha-512 -s)
+fi
+
+if [ ! -z "$TOOR_UID" ]; then
+  export TOOR_UID
 fi
 
 ansible-playbook -vv -i inventory site.yml
