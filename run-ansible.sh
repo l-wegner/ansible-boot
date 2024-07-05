b0175f8b5f1c1af44646fcfcdb19144bbe027891
Lars Wegner <lars.wegner.devs@gmail.com>
2024-07-05 17:51:12 +0200

    Fix run script

--- run-ansible.sh
+++ run-ansible.sh
@@ -11,16 +11,16 @@ source ${SCRIPT_LOC}/create-user.sh
 
 echo "switching to ansible user: ${TOOR_NM}"
 
-sudo -u "${TOOR_NM}" -- bash -c '
+sudo -i -u "${TOOR_NM}" --preserve-env=TOOR_NM,USER_NM <<EOF
 cd
 mkdir -p git && cd git
-pwd
 echo "linking the repo"
-#git clone git@github.com:l-wegner/ansible-boot.git
-sudo cp -r /home/larser/git/ansible-boot/ .
-sudo chown -R ${TOOR_NM}:${TOOR_NM} ansible-boot
-cd ansible-boot
+if [ -d ansible-boot ]; then
+  cd ansible-boot && git pull
+else
+  git clone https://github.com/l-wegner/ansible-boot.git && cd ansible-boot
+fi
 ansible-playbook -vv -i inventory site.yml --tags paru
 
-ansible-playbook -vv -i inventory site.yml --tags workstation,aur
-' --TOOR_NM=$TOOR_NM --USER_NM=$USER_NM
+ansible-playbook -vv -i inventory site.yml --tags workstation,aur 
+EOF
