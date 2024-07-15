581e2ee96805f6f61bd8d725aa72028ac01bd234
Lars Wegner <lars.wegner.devs@gmail.com>
2024-07-15 11:17:57 +0200

    Externalize sudo group

--- create-user.sh
+++ create-user.sh
@@ -4,14 +4,18 @@ function prompt_user() {
 USER_NM_DEF="lars"
 TOOR_NM_DEF="toor"
 TOOR_UID_DEF="1001"
+SUDO_GROUP_DEF="wheel"
 echo "Enter user name [${USER_NM_DEF}]: " && read USER_NM && \
 echo "Enter password for ${USER_NM:-${USER_NM_DEF}} user:" && read -s USER_PW && \
 echo "Enter ansible user name [${TOOR_NM_DEF}]: " && read TOOR_NM && \
 echo "Enter password for ansible user[${TOOR_NM:-${TOOR_NM_DEF}}]:" && read -s TOOR_PW && \
 echo "Enter UID and GID for ansible user [${TOOR_UID_DEF}]: " && read TOOR_UID
+echo "Enter sudo group name [${SUDO_GROUP_DEF}]: " && read SUDO_GROUP
+
 
 export USER_NM=${USER_NM:-${USER_NM_DEF}}
 export TOOR_NM=${TOOR_NM:-${TOOR_NM_DEF}}
+export SUDO_GROUP=${SUDO_GROOP:-${SUDO_GROUP_DEF}}
 if [ ! -z "$USER_PW" ]; then
   export USER_PW_HASH=$(echo $USER_PW | mkpasswd -m sha-512 -s)
 fi
